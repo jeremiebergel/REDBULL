154b0f416544b762271d1813271558409bde2425
masis-hetic <masis.gulmez@hetic.net>
2017-03-22 19:37:10 +0100

    [CHG] Index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once "inc/functions.php";
+require_once "espacemembre/inc/functions.php";
 // je démarre ma session
 session_start();
  /*on se connecte à la BDD*/
-require_once "inc/connect.php";
+require_once "espacemembre/inc/connect.php";
 
 // je vérifie si des données ont été postées, si cette variable n'est pas vide
 // ça signifie que des données on bien été postées.
@@ -151,9 +151,9 @@ if (!empty($_POST)){
 	function login() {
 		FB.login(function(response) {
 			if (response.status === 'connected') {
-	    		document.getElementById('status').innerHTML = 'We are connected.';
+	    		// document.getElementById('status').innerHTML = 'We are connected.';
 	    		document.getElementById('login').style.visibility = 'hidden';
-	    		document.location.href="http://www.monatest.esy.es/test.html";
+	    		document.location = "http://monatest.esy.es/redbull/test.html";
 	    	} else if (response.status === 'not_authorized') {
 	    		document.getElementById('status').innerHTML = 'We are not logged in.'
 	    	} else {
@@ -182,6 +182,9 @@ if (!empty($_POST)){
 			<p class="date">July 13th-16th, 2017</p>
 			<p class="cta"><a href="#">Star your custom</a></p>
 		</div>
+		<div class="create-acc">
+			BONJOUR
+		</div>
 	</main>
 
 	<div class="wrapper-popup closed">
@@ -200,13 +203,13 @@ if (!empty($_POST)){
 			</a>
 			<a class="social fb" href="#">
 			
-				<div><?xml version="1.0" encoding="UTF-8"?><!DOCTYPE PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" fill="#fff" /></svg></div>
+				<div><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" fill="#fff" /></svg></div>
 				<fb: class="btnbis" loggin-button scope="public_profile,email" onclick="login()" id="login"><p>FACEBOOK</p></fb: loggin-button>
 
 				<div id="fb-root"></div>
 			</a>
 			<a class="social twitter" href="#">
-				<div><?xml version="1.0" encoding="UTF-8"?><!DOCTYPE PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24" fill="#fff"><path d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" /></svg></div>
+				<div><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24" fill="#fff"><path d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" /></svg></div>
 				<p>Twitter</p>
 			</a>
 		</div>
