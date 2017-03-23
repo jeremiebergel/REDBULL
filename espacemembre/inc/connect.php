eb759d04e0da1a139c22375f51f55281b139d82d
mona <mona.calibani@hetic.net>
2017-03-23 14:22:06 +0100

    last version index.php

--- espacemembre/inc/connect.php
+++ espacemembre/inc/connect.php
@@ -1,7 +1,8 @@
 <?php
 
 try{
-    $pdo = new PDO('mysql:dbname=u107319540_bdd;host=mysql.hostinger.fr','u107319540_root','tamere');
+    //$pdo = new PDO('mysql:dbname=u107319540_bdd;host=mysql.hostinger.fr','u107319540_root','tamere');
+    $pdo = new PDO('mysql:dbname=cours_PHP;host=localhost','root','root');
 } catch(PDOException $exception) {
     die($exception->getMessage());
 }
